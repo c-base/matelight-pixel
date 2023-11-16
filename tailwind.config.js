1565072aa264285fe5259f3f757faa9ddf082854
Ramzi Khahil <ramzi.khahil@pm.me>
2023-11-16 22:41:30 +0100

    Removed unused files

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./templates/**/*.{html,js}", "./static/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
